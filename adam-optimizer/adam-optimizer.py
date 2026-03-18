b45905b16142e5f8a300c0656399f0387411a3a3
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-03-19 01:08:31 +0700

    Add solution for adam-optimizer

---
+++ adam-optimizer/adam-optimizer.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
+    """
+    One Adam optimizer update step.
+    Return (param_new, m_new, v_new).
+    """
+    # Write code here
+    param = np.array(param)
+    grad = np.array(grad)
+    m = np.array(m)
+    v = np.array(v)
+    m_new = beta1 * m + (1 - beta1) * grad
+    v_new = beta2 * v + (1 - beta2) * grad * grad
+    beta1_t, beta2_t = np.pow([beta1, beta2], t)
+    m_hat = m_new / (1 - beta1_t)
+    v_hat = v_new / (1 - beta2_t)
+    dparam = m_hat / (np.sqrt(v_hat) + eps)
+    param_new = param - lr * dparam
+    return param_new, m_new, v_new
