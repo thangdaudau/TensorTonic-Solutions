d91f8f98e80a0cc8aa87f4f22ce1703e8055b9ef
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-03-20 23:04:42 +0700

    Update solution for impute-missing

--- impute-missing/impute-missing.py
+++ impute-missing/impute-missing.py
@@ -1,44 +1,20 @@
 import numpy as np
 
 def impute_missing(X, strategy='mean'):
-    """
-    Fill NaN values in each feature column using column mean or median.
-    """
-    # Write code here
-    X = np.array(X)
-    if len(X.shape) == 1:
-        n_feature, = X.shape
-        if strategy == 'mean':
-            mean = np.mean([X[i] for i in range(n_feature) if not np.isnan(X[i])])
-            if np.isnan(mean):
-                mean = 0
-            for i in range(n_feature):
-                if np.isnan(X[i]):
-                    X[i] = mean
-        else:
-            med = np.median([X[i] for i in range(n_feature) if not np.isnan(X[i])])
-            if np.isnan(med):
-                med = 0
-            for i in range(n_feature):
-                if np.isnan(X[i]):
-                    X[i] = med
-    else:
-        n_sample, n_feature = X.shape
-        if strategy == 'mean':
-            for j in range(n_feature):
-                mean = np.mean([X[i, j] for i in range(n_sample) if not np.isnan(X[i, j])])
-                if np.isnan(mean):
-                    mean = 0
-                for i in range(n_sample):
-                    if X[i, j] != X[i, j]:
-                        X[i, j] = mean
-        else:
-            for j in range(n_feature):
-                med = np.median([X[i, j] for i in range(n_sample) if not np.isnan(X[i, j])])
-                if np.isnan(med):
-                    med = 0
-                for i in range(n_sample):
-                    if X[i, j] != X[i, j]:
-                        X[i, j] = med
+    X = np.asarray(X, dtype=float)
+
+    if X.ndim == 1:
+        func = np.nanmean if strategy == 'mean' else np.nanmedian
+        val = func(X)
+        if np.isnan(val):
+            val = 0
+        return np.where(np.isnan(X), val, X)
+
+    # 2D
+    func = np.nanmean if strategy == 'mean' else np.nanmedian
+    vals = func(X, axis=0)
+    vals = np.where(np.isnan(vals), 0, vals)
+
+    inds = np.where(np.isnan(X))
+    X[inds] = vals[inds[1]]
     return X
-    
