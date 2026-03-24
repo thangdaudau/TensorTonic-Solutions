f289fa90d46a24b5e128f103de8a4056bf7826c1
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-03-24 10:40:56 +0700

    Add solution for lstm-input-gate

---
+++ lstm/lstm-input-gate/lstm-input-gate.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
+
+def input_gate(h_prev: np.ndarray, x_t: np.ndarray,
+               W_i: np.ndarray, b_i: np.ndarray,
+               W_c: np.ndarray, b_c: np.ndarray) -> tuple:
+    """Compute input gate and candidate memory."""
+    # YOUR CODE HERE
+    hx = np.concat([h_prev, x_t], axis=1)
+    i_t = sigmoid(hx @ W_i.T + b_i)
+    C_t = np.tanh(hx @ W_c.T + b_c)
+    return i_t, C_t
