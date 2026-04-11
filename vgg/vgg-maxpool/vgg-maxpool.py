6f050e543432a4453071f05a161b8692baf7c12c
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-04-11 14:41:00 +0700

    Update solution for vgg-maxpool

--- vgg/vgg-maxpool/vgg-maxpool.py
+++ vgg/vgg-maxpool/vgg-maxpool.py
@@ -6,16 +6,4 @@ def vgg_maxpool(x: np.ndarray) -> np.ndarray:
     """
     # Your implementation here
     batches, h, w, c = x.shape
-    h_out, w_out, c_out = h // 2, w // 2, c
-    
-    y = np.ndarray((batches, h_out, w_out, c_out))
-    for b in range(batches):
-        for i in range(h_out):
-            for j in range(w_out):
-                y[b, i, j] = np.maximum.reduce([
-                    x[b, 2 * i, 2 * j],
-                    x[b, 2 * i + 1, 2 * j],
-                    x[b, 2 * i, 2 * j + 1],
-                    x[b, 2 * i + 1, 2 * j + 1]
-                ])
-    return y
+    return x.reshape(batches, h // 2, 2, w // 2, 2, c).max(axis=(2, 4))
