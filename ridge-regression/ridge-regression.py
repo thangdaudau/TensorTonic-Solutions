0796f576a886f4bd5710c6300ad4ed24b21786ae
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-04-12 17:57:21 +0700

    Add solution for ridge-regression

---
+++ ridge-regression/ridge-regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def ridge_regression(X, y, lam):
+    """
+    Compute ridge regression weights using the closed-form solution.
+    """
+    # Write code here
+    X = np.array(X)
+    y = np.array(y)
+    N, d = X.shape
+    return np.linalg.inv(X.T @ X + lam * np.eye(d)) @ X.T @ y.T
