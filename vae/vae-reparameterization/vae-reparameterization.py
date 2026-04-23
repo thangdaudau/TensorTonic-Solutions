4bbad903ede8c2723d9584651a1bdd1a1931a7b7
peapea <54851828+thangdaudau@users.noreply.github.com>
2026-04-23 19:21:43 +0700

    Add solution for vae-reparameterization

---
+++ vae/vae-reparameterization/vae-reparameterization.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def reparameterize(mu: np.ndarray, log_var: np.ndarray, epsilon: np.ndarray) -> np.ndarray:
+    """
+    Returns: np.ndarray z of shape (batch, latent_dim) sampled via reparameterization
+    """
+    # Your implementation here
+    z = mu + np.exp(0.5 * log_var) * epsilon
+    return z
